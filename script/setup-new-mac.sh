934a28197d8a1f88a58cdd89ab7c4322b1699e62
Lyn Nagara <lyn.nagara@gmail.com>
2019-04-25 12:25:54 -0700

    remove outdated stuff

--- script/setup-new-mac.sh
+++ script/setup-new-mac.sh
@@ -27,25 +27,6 @@ chsh -s /bin/zsh
 brew install git
 echo "export PATH=/usr/local/bin:$PATH" >> ~/.bash_profile
 
-# Node
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.26.1/install.sh | bash
-nvm install iojs
-nvm install node
-
-# Ruby
-brew install gpg2
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-\curl -sSL https://get.rvm.io | bash -s stable --ruby
-source $HOME/.bash_profile
-
-# Python
-sudo easy_install pip
-
-
-# Haskell
-brew cask install haskell-platform
-cabal install cabal-install
-
 # Mac apps
 printf "installing Mac applications..."
 brew cask install google-chrome firefox sublime-text3 heroku-toolbelt dropbox vlc skype spotify spotify-notifications
